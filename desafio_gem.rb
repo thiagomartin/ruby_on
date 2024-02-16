fd50bee1c30d636aa5bcdb9e37f9469a0d5832f6
thiagomartin <155474906+thiagomartin@users.noreply.github.com>
2024-02-16 11:37:16 -0300

    ok

--- desafio_gem.rb
+++ desafio_gem.rb
@@ -16,3 +16,21 @@ puts "O resultado de #{base} elevado a #{expoente} é : #{resultado}."
 
 #Siga a documentação da gem CPF_CNPJ para criar um programa que recebe como entrada um número de CPF e que um método
 #verifique se esta numero é válido
+
+require 'cpf_cnpj'
+
+
+def cpf_valido?(cpf)
+  CPF.valid?(cpf)
+end
+
+puts "Digite o CPF para verificação:"
+cpf_digitado = gets.chomp
+
+
+if cpf_valido? (cpf_digitado)
+    cpf_formatado = CPF.new(cpf_digitado).formatted
+  puts "#{cpf_formatado} é um CPF  válido!"
+else
+    puts "#{cpf_formatado} não é um CPF válido!"
+end
