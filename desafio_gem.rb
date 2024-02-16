5ce2b40931e9446aa5ae3868e4f1813a663b05eb
thiagomartin <155474906+thiagomartin@users.noreply.github.com>
2024-02-16 12:03:42 -0300

    melhorando o programa cpf

--- desafio_gem.rb
+++ desafio_gem.rb
@@ -24,13 +24,27 @@ def cpf_valido?(cpf)
   CPF.valid?(cpf)
 end
 
-puts "Digite o CPF para verificação:"
-cpf_digitado = gets.chomp
+loop do
+  puts "\n\n Escolha a opção:"
+  puts "1. Verificar CPF"
+  puts "2. Sair"
+  opção = gets.chomp.to_i
 
+  case opção
+  when 1
+    puts "Digite  o CPF para verificação:"
+    cpf_digitato = gets.chomp
 
-if cpf_valido? (cpf_digitado)
-    cpf_formatado = CPF.new(cpf_digitado).formatted
-  puts "#{cpf_formatado} é um CPF  válido!"
-else
-    puts "#{cpf_formatado} não é um CPF válido!"
+    if cpf_valido?(cpf_digitato)
+      cpf_formatado = CPF.new(cpf_digitato).formatted
+      puts "#{cpf_formatado} é valido!"
+    else
+        puts "#{cpf_formatado} não é um CPF válido"
+    end
+when 2
+    puts "Saindo do programa. Até logo!"
+        break
+    else
+        puts "Opção invalida. Por favor , escolha uma opção válida."
+  end
 end
