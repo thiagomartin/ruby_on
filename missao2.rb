5ccc7906c1656f999718e815a59eb5b069cf0dde
thiagomartin <155474906+thiagomartin@users.noreply.github.com>
2024-02-20 10:02:01 -0300

    ok

--- missao2.rb
+++ missao2.rb
@@ -4,3 +4,37 @@
 #pessoa juridica adicionada
 #nome: M.C Investimentos
 #cnpj 4241.123/0001
+
+
+module Person
+  class Juridic
+    attr_accessor :name, :document
+     def initialize(name, cnpj)
+       @name = name
+       @cnpj = cnpj
+     end
+
+     def add
+       puts "Pessoa Jurídica Adicionada"
+       puts "Nome: #{@name}"
+       puts "Cnpj: #{@cnpj}"
+     end
+  end
+#Aqui quiz ir alem do que foi pedido na questao anterior e criar uma classe Physical herdando da classe Juridic
+  class Physical < Juridic
+    attr_accessor :cpf
+
+    def initialize(name, cpf)
+      super(name, nil)
+      @cpf = cpf
+    end
+    def add
+      puts "Pessoa Física Adicionada"
+      puts "Nome:  #{@name}"
+      puts "CPF: #{@cpf}"
+    end
+  end
+  end
+
+  Person::Juridic.new("M.C Investimentos", "4241.123/0001").add
+  Person::Physical.new("Thiago Martin Andriolo", "36052057807").add
