5299999d7b3c703aae1eb9977e922542bda17ccd
thiagomartin <155474906+thiagomartin@users.noreply.github.com>
2024-02-20 16:27:37 -0300

    ok

---
+++ missao_II_2.rb
@@ -0,0 +1,3 @@
+#Em uma classe cjamada Carro, crie um método publico camado get_Km que recebe como parâmetro a seguinte informação
+#"Um fusca de cor amarela viaja a 80 Km/h".
+#o metodo get_km deve chamar um método privado com o nome find_km. no final, imprima este retorno
---
+++ missão_II_1.rb
@@ -0,0 +1,2 @@
+#crie uma expressão que faça o casamento de padrao com o numero de telefone presente no texto a seguir
+#"Olá, tudo bem? Meu Whats é (99) 74321-1234"
