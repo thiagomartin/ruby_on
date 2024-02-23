352d1f3e528ffc14521b98c8e8eac92bde6a3286
thiagomartin <155474906+thiagomartin@users.noreply.github.com>
2024-02-23 09:20:32 -0300

    corrigindo texto

--- missão_II_1.rb
+++ missão_II_1.rb
@@ -1,7 +1,7 @@
 #crie uma expressão que faça o casamento de padrao com o numero de telefone presente no texto a seguir
 #"Olá, tudo bem? Meu Whats é (99) 74321-1234"
 
-text = "Olpa, tudo bem? Meu whats é (99) 74321-1234."
+text = "Olá, tudo bem? Meu whats é (99) 74321-1234."
 
 padrao_telefone = /\(\d{2}\) \d{5}-\d{4}/
 
